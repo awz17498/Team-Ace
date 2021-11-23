20470a415c2b72e85cff31119699c8a46e4dd891
송환욱 <shu990810@naver.com>
2021-11-23 20:38:20 +0900

    주석

--- Ace_Project/Ace.py
+++ Ace_Project/Ace.py
@@ -6,29 +6,29 @@ from pygame.locals import *
 
 class Main(object):
     def start(self):
-        pygame.init() # ← pygame 초기화
-        screen = pygame.display.set_mode((770, 420)) # ← 프로그램 해상도 설정
-        pygame.display.set_caption("ACE_오파고") # ← 프로그램 제목 설정
+        pygame.init() # pygame 초기화
+        screen = pygame.display.set_mode((770, 420)) # 프로그램 해상도 설정
+        pygame.display.set_caption("ACE_오파고") # 파이게임 제목 설정
 
         board = Board()
         ai = AI("AI", "○")
         rule = Rule()
-        turn = True # ← AI 차례 = False , user 차례 = True
+        turn = True # AI 차례 = False / user 차례 = True
         count = 1
         
         temp_x = 0
         temp_y = 0
 
-        rand_x = random.randrange(8, 12)
-        rand_y = random.randrange(8, 12)
+        rand_x = random.randrange(8, 12) # 8 이상 12 미만의 난수 중 하나를 무작위로 얻음(X좌표)
+        rand_x = rand_x * 21 + 10 # (8 x 21 + 10 = 178), (9 x 21 + 10 = 199), (10 x 21 + 10 = 220), (11 x 21 + 10 = 241) 무작위로 나올 수 있는 난수들을 공식에 대입한 값
 
-        rand_x = rand_x * 21 + 10
-        rand_y = rand_y * 21 + 10
+        rand_y = random.randrange(8, 12) # 8 이상 12 미만의 난수 중 하나를 무작위로 얻음(Y좌표)
+        rand_y = rand_y * 21 + 10 # (8 x 21 + 10 = 178), (9 x 21 + 10 = 199), (10 x 21 + 10 = 220), (11 x 21 + 10 = 241) 무작위로 나올 수 있는 난수들을 공식에 대입한 값
 
         black_stones = [[rand_x, rand_y]]
         white_stones = []
 
-        end_check = False # ← 최초 실행 시 승리 판정을 False로 하여 진행 가능
+        end_check = False # 최초 실행 시 승리 판정을 False로 하여 진행 가능
 
         while True:
             user_select_x = False
@@ -40,33 +40,42 @@ class Main(object):
                     pygame.quit()
                     sys.exit()
 
-                LEFT = 1   # ← 왼쪽 버튼에 대한 버튼 인덱스
+                LEFT = 1 # 왼쪽 버튼에 대한 버튼 인덱스
 
                 # 좌클릭시 놓아지는 부분
-                if event.type == MOUSEBUTTONUP and event.button == LEFT:
+                if event.type == MOUSEBUTTONUP and event.button == LEFT: # 마우스 버튼을 누른 후 뗄 때 발생함 + 마우스 좌클릭(AND)
+                    '''
+                    pygame.mouse.get_pos()
+                    마우스의 포지션 x, y 값을 튜플로 반환한다.
+                    pygame.mouse.get_pos() -> (123, 345)
+                    '''
                     mouse_xy = pygame.mouse.get_pos()
-                    x = (mouse_xy[0] - 10) // 21
-                    y = (mouse_xy[1] - 10) // 21
-                    user_select_x = y
-                    user_select_y = x
+                    '''
+                    소수점 이하의 수를 모두 버리고 몫만 나타낼 때 ‘//’ 연산자를 사용
+                    ''' 
+                    print(pygame.mouse.get_pos())
+                    x = (mouse_xy[0] - 10) // 21 # 미해석
+                    y = (mouse_xy[1] - 10) // 21 # 미해석
+                    user_select_x = y # 미해석
+                    user_select_y = x # 미해석
 
                 # 커서가 안내하는 부분
-                elif event.type == MOUSEMOTION:
+                elif event.type == MOUSEMOTION: # 마우스가 움직일 때 발생함
                     mouse_xy = pygame.mouse.get_pos()
-                    x = (mouse_xy[0] - 10) // 21
-                    y = (mouse_xy[1] - 10) // 21
-                    temp_x = x * 21 + 10
-                    temp_y = y * 21 + 10
+                    x = (mouse_xy[0] - 10) // 21 # 미해석
+                    y = (mouse_xy[1] - 10) // 21 # 미해석
+                    temp_x = x * 21 + 10 # 미해석
+                    temp_y = y * 21 + 10 # 미해석
 
             # 게임의 상태를 업데이트하는 부분
-            if turn == False:
+            if turn == False: # AI의 턴일 때
                 x, y = ai.select_stone(board)
                 gui_x = y * 21 + 10
                 gui_y = x * 21 + 10
                 board.put_stone(x, y, 11)
                 black_stones.append([gui_x,gui_y])
                 if rule.win_check(x, y, board, 11):
-                    end_check = 'black' # ← AI의 승리
+                    end_check = 'black' # AI의 승리
                 count += 1
                 turn = not turn
             else:
@@ -79,43 +88,44 @@ class Main(object):
                             white_stones.append([gui_x, gui_y])
                             board.put_stone(x, y, 10)
                             if rule.win_check(x, y, board, 10):
-                                end_check = 'white' # ← User의 승리
+                                end_check = 'white' # User의 승리
                             count += 1
                             turn = not turn
                         else:
                             three_x_three_warning = True
 
             # 게임의 상태를 화면에 그려주는 부분 -> 화면을 지우고 업데이트하는 코드
-            screen.fill((255, 255, 255)) # ← 공백 RGB 지정
-            screen.blit(pygame.image.load('Image/omok_board.jpg'), (0, 0)) # ← '오목판'IMG 출력
-            screen.blit(pygame.image.load('Image/rule.jpg'), (420, 0)) # ← '룰'IMG 출력
+            screen.fill((255, 255, 255)) # 공백 RGB 지정
+            screen.blit(pygame.image.load('Image/omok_board.jpg'), (0, 0)) # '오목판'IMG 출력
+            screen.blit(pygame.image.load('Image/rule.jpg'), (420, 0)) # '룰'IMG 출력
 
-            for st in white_stones: # ← 백돌 RGB, SIZE 지정
+            for st in white_stones: # 백돌 RGB, SIZE 지정
                 pygame.draw.circle(screen, (250, 250, 250), st, 10)
 
-            for st in black_stones: # ← 흑돌 RGB, SIZE 지정
+            for st in black_stones: # 흑돌 RGB, SIZE 지정
                 pygame.draw.circle(screen, (0, 0, 0), st, 10)
 
-            pygame.draw.circle(screen,(120,120,120),black_stones[-1], 10) # ← 마지막 흑돌 RGB, SIZE 지정
+            pygame.draw.circle(screen,(120,120,120),black_stones[-1], 10) # 마지막 흑돌 RGB, SIZE 지정
 
-            if three_x_three_warning: # ← 3x3 경고 트리거
-                screen.blit(pygame.image.load('Image/33.jpg'),(200, 100)) # ← '3x3 경고'IMG 출력
+            if three_x_three_warning: # 3x3 경고 트리거
+                screen.blit(pygame.image.load('Image/33.jpg'),(200, 100)) # '3x3 경고'IMG 출력
                 pygame.display.update()
-                time.sleep(1) # ← 타이머 1초
+                time.sleep(1) # 타이머 1초
 
-            if temp_x <= 420 and temp_y <= 420: # ← 마우스의 좌표가 오목 판 내에 있을 때 / 착수 전 백돌 RGB와 SIZE 지정
+            if temp_x <= 420 and temp_y <= 420: # 마우스의 좌표가 오목 판 내에 있을 때 / 착수 전 백돌 RGB와 SIZE 지정
                 pygame.draw.circle(screen, (220, 220, 220),[temp_x,temp_y], 10)
 
-            if end_check == 'black': # ← 만약 AI의 승리 판정일 때
+            if end_check == 'black': # 만약 AI의 승리 판정일 때
                 for i in range(5, 0, -1):
-                    screen.blit(pygame.image.load('Image/ai'+str(i)+'.jpg'), (465, 55))
+                    screen.blit(pygame.image.load('Image/ai'+str(i)+'.jpg'), (465, 55)) # Image폴더의 ai1/2/3/4/5를 문자열에서 정수형으로 변환함
                     pygame.display.update()
-                    time.sleep(1) # ← 타이머 1초
-            elif end_check == 'white': # ← 만약 유저의 승리 판정일 때
+                    time.sleep(1) # 이미지 사진을 1초 딜레이 주어 표현(총 5장 = 5초)
+
+            elif end_check == 'white': # 만약 유저의 승리 판정일 때
                 for i in range(5, 0, -1):
-                    screen.blit(pygame.image.load('Image/user'+str(i)+'.jpg'),(465, 55))
+                    screen.blit(pygame.image.load('Image/user'+str(i)+'.jpg'),(465, 55)) # Image폴더의 user1/2/3/4/5를 문자열에서 정수형으로 변환함
                     pygame.display.update()
-                    time.sleep(1) # ← 타이머 1초
+                    time.sleep(1) # 이미지 사진을 1초 딜레이 주어 표현(총 5장 = 5초)
             else:
                 pygame.display.update()
 
@@ -128,7 +138,7 @@ class Main(object):
             '''
 
 class Board(object):
-    def __init__(self): # ← 오목판
+    def __init__(self): # 오목판
         self.omok_board = [[1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3],
                            [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6],
                            [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6],
@@ -159,7 +169,7 @@ class Board(object):
         self.omok_board[x][y] = color
 
 class Offset(object):
-    def __init__(self): # ← 3시부터 시계 방향으로 8방향
+    def __init__(self): # 3시부터 시계 방향으로 8방향
         self.offset = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
 
 class Rule(Offset):
